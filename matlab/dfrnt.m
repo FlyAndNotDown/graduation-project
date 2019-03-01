a91b6bf243f60d5870fa6547ebf006ec5d846be6
John Kindem <461425614@qq.com>
2019-03-01 19:11:17 +0800

    🛠 fix a bug

--- matlab/dfrnt.m
+++ matlab/dfrnt.m
@@ -1,26 +1,23 @@
-function [result, decodeEig] = dfrnt(source, a, m)
+function result = dfrnt(source, a, m, p)
 %dfrnt 1-D discrete fractional random transform
 %
 % - Description:
 %       this function can do a 1-D discrete fractional random transform,
 %       it input a 1xN size matrix, the order of transform, the cycle of
-%       transform and return a 1xN size matrix
+%       transform, a random matrix and return a 1xN size matrix
 %
 % - Arguments:
 %       - source [1xN matrix] the source matrix with 1xN size
 %       - a [float number] the order of transform
 %       - m [integer] the cycle of transform
+%       - p [NxN matrix] a random matrix, N is cols of source
 % 
 % - Returns:
 %       - result [1xN matrix] the result matrix with 1xN size
-%       - decodeEig [1xN matrix] the eigenvector matrix use to decode
 %
 
 % get size of source matrix
 [~, sourceCols] = size(source);
-
-% get a random matrix 'p'
-p = rand(sourceCols, sourceCols);
 
 % get the matrix 'q', q = (p + p') / 2
 q = (p + p') / 2;
