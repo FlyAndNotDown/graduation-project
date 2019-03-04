60995b4308dbfa49c95c29c311ab6ab870ead069
John Kindem <461425614@qq.com>
2019-03-04 23:05:48 +0800

    🎉 add a new test script

---
+++ matlab/test/lqdfrntTest.m
@@ -0,0 +1,13 @@
+source = zeros(1, 4, 4);
+source(:, :, 1) = [0, 0, 0, 0];
+source(:, :, 2) = [1, 2, 3, 4];
+source(:, :, 3) = [2, 3, 4, 5];
+source(:, :, 4) = [3, 4, 5, 6];
+
+p = rand(4, 4);
+u = [0, 1, 0, 0];
+
+output = lqdfrnt(source, 0.25, 1, p, u);
+output4 = lqdfrnt(output, 0.25, 1, p, u);
+output4 = lqdfrnt(output4, 0.25, 1, p, u);
+output4 = lqdfrnt(output4, 0.25, 1, p, u);
