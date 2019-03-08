d85f2dc7d6bd2b8ce3023a6140eff7ab0ef289d1
John Kindem <461425614@qq.com>
2019-03-08 20:01:54 +0800

    🎉 add some code

--- matlab/function/mask/grainMask.m
+++ matlab/function/mask/grainMask.m
@@ -1,2 +1,23 @@
-function mask = grainMask(pixle, )
+function mask = grainMask(source)
+%grainMask - get grain mark of a pixel
+%
+% - Description:
+%       get grain mark value of a pixel
+%
+% - Arguments:
+%       - source [nxnx3 matrix] target(center) pixel and adjacent pixels' RBG matrix
+%
+% - Returns:
+%       - mask [double] grain mask value of target pixel
+
+% get size info
+[sourceRow, sourceCol] = size(source);
+
+% get center position
+centerRow = fix(sourceRow / 2) + 1;
+centerCol = fix(sourceCol / 2) + 1;
+
+% get sum of all pixel
+
+
 end
