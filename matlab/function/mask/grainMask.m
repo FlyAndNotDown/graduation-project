d725009915497ea27ebedfac8b06bd905c1dbd28
John Kindem <461425614@qq.com>
2019-03-09 20:39:05 +0800

    🎉 add some code

--- matlab/function/mask/grainMask.m
+++ matlab/function/mask/grainMask.m
@@ -1,14 +1,14 @@
-function mask = grainMask(source)
-%grainMask - get grain mark of a pixel
+function mask = textureMask(source)
+%textureMask - get texture mark value of a pixel
 %
 % - Description:
-%       get grain mark value of a pixel
+%       get texture mark value of a pixel
 %
 % - Arguments:
 %       - source [nxnx3 matrix] target(center) pixel and adjacent pixels' RBG matrix
 %
 % - Returns:
-%       - mask [double] grain mask value of target pixel
+%       - mask [double] texture mask value of target pixel
 
 % get size info
 [sourceRow, sourceCol] = size(source);
@@ -17,7 +17,25 @@ function mask = grainMask(source)
 centerRow = fix(sourceRow / 2) + 1;
 centerCol = fix(sourceCol / 2) + 1;
 
-% get sum of all pixel
+% get texture mask value of three channel
+sumR = 0;
+sumG = 0;
+sumB = 0;
+for n1 = 1 : sourceRow
+    for n2 = 1 : sourceCol
+        sumR = sumR + source(n1, n2, 1);
+        sumG = sumG + source(n1, n2, 2);
+        sumB = sumB + source(n1, n2, 3);
+    end
+end
+averageR = sumR / (sourceRow * sourceCol);
+averageG = sumG / (sourceRow * sourceCol);
+averageB = sumB / (sourceRow * sourceCol);
+textureR = abs(source(centerRow, centerCol, 1) - averageR);
+textureG = abs(source(centerRow, centerCol, 2) - averageG);
+textureB = abs(source(centerRow, centerCol, 3) - averageB);
 
+% get max value of three channel
+mask = max([textureR, textureG, textureB]);
 
 end
