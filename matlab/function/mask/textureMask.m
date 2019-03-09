9e7759a658238d1295ad065aed7c447db3cd8374
John Kindem <461425614@qq.com>
2019-03-09 22:04:27 +0800

    ✨ some refactor

--- matlab/function/mask/textureMask.m
+++ matlab/function/mask/textureMask.m
@@ -1,11 +1,12 @@
-function mask = textureMask(source)
-%textureMask - get texture mark value of a pixel
+function mask = textureMask(source, l)
+%textureMask - get texture mark value of a image block
 %
 % - Description:
-%       get texture mark value of a pixel
+%       get texture mark value of a image block
 %
 % - Arguments:
-%       - source [nxnx3 matrix] target(center) pixel and adjacent pixels' RBG matrix
+%       - source [nxnx3 matrix] source image block
+%       - l [integer] window length
 %
 % - Returns:
 %       - mask [double] texture mask value of target pixel
@@ -13,29 +14,40 @@ function mask = textureMask(source)
 % get size info
 [sourceRow, sourceCol, ~] = size(source);
 
-% get center position
-centerRow = fix(sourceRow / 2) + 1;
-centerCol = fix(sourceCol / 2) + 1;
+% get window size
+windowSize = (2 * l + 1) * (2 * l + 1);
 
-% get texture mask value of three channel
-sumR = 0;
-sumG = 0;
-sumB = 0;
+% for every pixel, calculate its texture mask value
+mask = 0
 for n1 = 1 : sourceRow
     for n2 = 1 : sourceCol
-        sumR = sumR + source(n1, n2, 1);
-        sumG = sumG + source(n1, n2, 2);
-        sumB = sumB + source(n1, n2, 3);
+        % get near (2l + 1)^2 size pixels' pixel sum
+        sumR = 0;
+        sumG = 0;
+        sumB = 0;
+        for n3 = (n1 - l) : (n1 + l)
+            for n4 = (n2 - l) : (n2 - l)
+                if n3 > 1 && n3 < sourceRow && n4 > 1 && n4 < sourceCol
+                    sumR = sumR + source(n3, n4, 1);
+                    sumG = sumG + source(n3, n4, 2);
+                    sumB = sumB + source(n3, n4, 3);
+                end
+            end
+        end
+
+        % get average
+        averageR = sumR / windowSize;
+        averageG = sumG / windowSize;
+        averageB = sumB / windowSize;
+
+        % get texture mask value in three channel
+        textureR = abs(source(n1, n2, 1) - averageR);
+        textureG = abs(source(n1, n2, 2) - averageG);
+        textureB = abs(source(n1, n2, 3) - averageB);
+
+        % get mask and add it to result
+        mask = mark + max([textureR, textureG, textureB]);
     end
 end
-averageR = sumR / (sourceRow * sourceCol);
-averageG = sumG / (sourceRow * sourceCol);
-averageB = sumB / (sourceRow * sourceCol);
-textureR = abs(source(centerRow, centerCol, 1) - averageR);
-textureG = abs(source(centerRow, centerCol, 2) - averageG);
-textureB = abs(source(centerRow, centerCol, 3) - averageB);
-
-% get max value of three channel
-mask = max([textureR, textureG, textureB]);
 
 end
