c39310a1e9102e2cae8903795cf8910c165b5773
John Kindem <461425614@qq.com>
2019-03-11 18:58:02 +0800

    🎉 add some code

--- matlab/function/watermark/qdfrntMark.m
+++ matlab/function/watermark/qdfrntMark.m
@@ -27,6 +27,17 @@ blocks = splitBlock(source, 8);
 u = [0, 1, 0, 0];
 
 % do QDFRNT to every blocks
-% TODO
+encodedBlocks = cell(1, blocksLength);
+for n = 1 : blocksLength
+    [blockRow, blockCol, blockHeight] = size(blocks{1, n});
+    t = zeros(blockRow, blockCol, blockHeight + 1);
+    for n = 2 : 4
+        t(:, :, n) = blocks{1, n}(:, :, n - 1);
+    end
+    encodedBlocks{1, n} = lqdfrnt2(t, kt, kt, u);
+end
+
+% get adaptive factor of every block
+adaptiveFactors = adaptiveFactor(blocks, 1);
 
 end
