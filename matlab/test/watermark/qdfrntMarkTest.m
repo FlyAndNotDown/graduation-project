5c7ca334f1dac8c992864497d6d4ea67004ad4b5
John Kindem <461425614@qq.com>
2019-03-12 14:37:49 +0800

    🛠 fix a bug

--- matlab/test/watermark/qdfrntMarkTest.m
+++ matlab/test/watermark/qdfrntMarkTest.m
@@ -8,7 +8,7 @@ r = dfrntKernel(0.25, 1, p);
 ir = dfrntKernel(-0.25, 1, p);
 
 % watermarking
-output = qdfrntMark(source, secret, 3, r, ir, 0.05);
+output = qdfrntMark(source, secret, 3, r, ir, 2);
 
 % show result
 figure();
