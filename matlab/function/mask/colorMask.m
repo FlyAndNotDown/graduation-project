a8373d1df71bca2b2f36322427658a1a9007358c
John Kindem <461425614@qq.com>
2019-03-24 01:33:58 +0800

    🎉 add some code

--- matlab/function/mask/colorMask.m
+++ matlab/function/mask/colorMask.m
@@ -1,19 +1,24 @@
-function mask = colorMask(source, sigma)
+function mask = colorMask(source, s)
 %colorMask - get color mask value of a image block
 %
 % - Arguments:
-%       - source [nxnx3 matrix] source image block
-%       - l [integer] window length
+%       - source [nxnx3 matrix] source image block with CIELab color space after normalization
+%       - s [double] a value has effect on transform
 %
 % - Returns:
 %       - mask [double] color mask value of target block
 
 % get size info
-[sourceRow, ~, ~] = size(source);
+[sourceRow, sourceCol, ~] = size(source);
 
-% change color space from RGB to CIELab
-sourceLab = rgb2lab(source);
-
-% TODO
+% for every pixel, calculate its color mask value and get sum
+mask = 0;
+for n1 = 1 : sourceRow
+    for n2 = 1 : sourceCol
+        channelA = source(n1, n2, 2);
+        channelB = source(n1, n2, 3);
+        mask = mask + 1 - exp(-(channelA ^ 2 + channelB ^ 2) / (s ^ 2));
+    end
+end
 
 end
