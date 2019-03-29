5bd64f1af528bff98610e150fb5c46600737706b
John Kindem <461425614@qq.com>
2019-03-29 12:47:28 +0800

    🎉 add some code

---
+++ matlab/test/watermark/qdfrntTrainTest.m
@@ -0,0 +1,12 @@
+% load image
+source = im2double(imread('lena.bmp'));
+
+% init secret sequence
+secret = rand(1, 200);
+
+% calculate the kernel of DFRNT
+p = rand(8, 8);
+r = dfrntKernel(0.75, 1, p);
+
+% start train
+model = qdfrntTrain(source, secret, r, 0.05);
