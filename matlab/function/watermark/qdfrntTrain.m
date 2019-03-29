42bf8ae2df871c53dc36750eea1b25252c0f794b
John Kindem <461425614@qq.com>
2019-03-29 12:49:23 +0800

    🛠 fix a bug

--- matlab/function/watermark/qdfrntTrain.m
+++ matlab/function/watermark/qdfrntTrain.m
@@ -28,7 +28,7 @@ blocks = splitBlock(source, 8);
 u = [0, 1, 0, 0];
 
 % get adaptive factors of every blocks
-adaptiveFactors = adaptiveFactors(blocks, 1, 0.25);
+adaptiveFactors = adaptiveFactor(blocks, 1, 0.25);
 
 % do QDFRNT to every blocks
 encodedBlocks = cell(1, blocksLength);
