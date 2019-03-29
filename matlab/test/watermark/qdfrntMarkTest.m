3ca439ac4a513020da021085f529a6be2669aa15
John Kindem <461425614@qq.com>
2019-03-29 13:19:53 +0800

    🎉 add some code

--- matlab/test/watermark/qdfrntMarkTest.m
+++ matlab/test/watermark/qdfrntMarkTest.m
@@ -3,48 +3,18 @@ source = im2double(imread('lena.bmp'));
 secret = imread('secret.bmp');
 
 % calculate the kernel of DFRNT
-p1 = rand(8, 8);
-p2 = rand(8, 8);
-p3 = rand(8, 8);
-r1 = dfrntKernel(0.25, 1, p1);
-ir1 = dfrntKernel(-0.25, 1, p1);
-r2 = dfrntKernel(0.25, 2, p2);
-ir2 = dfrntKernel(-0.25, 2, p2);
-r3 = dfrntKernel(0.75, 1, p3);
-ir3 = dfrntKernel(-0.75, 1, p3);
+p = rand(8, 8);
+r = dfrntKernel(0.75, 1, p);
+ir = dfrntKernel(-0.75, 1, p);
 
 % watermarking
-output1 = qdfrntMark(source, secret, 3, r1, ir1, 0.005);
-output2 = qdfrntMark(source, secret, 3, r1, ir1, 0.02);
-output3 = qdfrntMark(source, secret, 3, r2, ir2, 0.005);
-output4 = qdfrntMark(source, secret, 3, r2, ir2, 0.02);
-output5 = qdfrntMark(source, secret, 3, r3, ir3, 0.005);
-output6 = qdfrntMark(source, secret, 3, r3, ir3, 0.02);
-output7 = qdfrntMark(source, secret, 3, r3, ir3, 0.05);
+[output, kp] = qdfrntMark(source, secret, 3, r, ir, 0.05);
 
-[ssimVal1, ~] = ssim(source, output1);
-[ssimVal2, ~] = ssim(source, output2);
-[ssimVal3, ~] = ssim(source, output3);
-[ssimVal4, ~] = ssim(source, output4);
-[ssimVal5, ~] = ssim(source, output5);
-[ssimVal6, ~] = ssim(source, output6);
-[ssimVal7, ~] = ssim(source, output7);
+[ssimVal, ~] = ssim(source, output);
 
 % show result
 figure();
-subplot(2, 4, 1);
+subplot(1, 2, 1);
 imshow(source);
-subplot(2, 4, 2);
-imshow(output1);
-subplot(2, 4, 3);
-imshow(output2);
-subplot(2, 4, 4);
-imshow(output3);
-subplot(2, 4, 5);
-imshow(output4);
-subplot(2, 4, 6);
-imshow(output5);
-subplot(2, 4, 7);
-imshow(output6);
-subplot(2, 4, 8);
-imshow(output7);
+subplot(1, 2, 2);
+imshow(output);
