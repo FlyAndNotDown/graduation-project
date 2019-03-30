945ce44e78688d2a29e0c3b24b6f400fddb2c73f
John Kindem <461425614@qq.com>
2019-03-31 01:18:04 +0800

    🎉 add some code

--- application/frontend/src/index.tsx
+++ application/frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { MainRouter } from './router';
+import './css/css';
 
 ReactDom.render(
     <MainRouter/>,
--- application/frontend/src/tool/css.ts
+++ application/frontend/src/tool/css.ts
@@ -1,3 +1,28 @@
-export class CssTool {
+interface Style {
+    [propName: string]: string
+}
 
+export class CssTool {
+    static combClassName(defaultClassName: string, inputClassName: string): string {
+        return inputClassName ? `${defaultClassName} ${inputClassName}` : defaultClassName
+    }
+
+    static combStyle(defaultStyle: Style, inputStyle: Style): Style {
+        let style: Style = {};
+        if (defaultStyle) {
+            for (let key in defaultStyle) {
+                if (<Object>defaultStyle.hasOwnProperty(key)) {
+                    style[key] = defaultStyle[key];
+                }
+            }
+        }
+        if (inputStyle) {
+            for (let key in inputStyle) {
+                if (<Object>inputStyle.hasOwnProperty(key)) {
+                    style[key] = inputStyle[key];
+                }
+            }
+        }
+        return style;
+    }
 }
