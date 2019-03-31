04458e46a7a97dbfe17adb166b3c64802f2d1fde
John Kindem <461425614@qq.com>
2019-03-31 14:44:17 +0800

    🎉 add some code

---
+++ application/backend/src/server.ts
@@ -0,0 +1,11 @@
+import * as Koa from 'koa';
+import * as routerGenerator from 'koa-router';
+
+export class Server {
+    private server: any;
+    private router: any;
+    
+    constructor() {}
+
+    private init(): void {}
+}
