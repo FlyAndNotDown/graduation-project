a2b4875e3b9fa653bdf5077299b748ad659c1b09
John Kindem <461425614@qq.com>
2019-03-31 23:15:23 +0800

    🎉 add some code

--- application/backend/src/server.ts
+++ application/backend/src/server.ts
@@ -1,5 +1,7 @@
+import { Log } from './tool/log';
 import * as Koa from 'koa';
-import * as routerGenerator from 'koa-router';
+import * as Router from 'koa-router';
+import { serverConfig } from './config';
 
 export class Server {
     private server: any;
@@ -7,5 +9,28 @@ export class Server {
     
     constructor() {}
 
-    private init(): void {}
+    private init(): void {
+        Log.log('server was init.');
+
+        this.server = new Koa();
+        this.server.keys = serverConfig.keys;
+        this.router = new Router();
+    }
+
+    private loadController(): void {}
+
+    private loadMiddleware(): void {}
+
+    private listen(): void {}
+
+    public start() {
+        this.init();
+        this.loadMiddleware();
+        this.loadController();
+        this.listen();
+        
+        Log.log('server is running ...');
+
+        return this;
+    }
 }
