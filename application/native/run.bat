aba9beaf87a5185413272d77308051702649769e
John Kindem <461425614@qq.com>
2019-04-01 12:50:19 +0800

    ✨ some refactor

--- application/native/run.bat
+++
@@ -1,8 +0,0 @@
-set opencvJarPath=D:\Lib\OpenCV\opencv\build\java\opencv-401.jar
-set opencvJavaLibPath=D:\Lib\OpenCV\opencv\build\java\x64
-set opencvBinPath=D:\Lib\OpenCV\opencv\build\x64\vc15\bin
-set srcLocation=D:\Code\projects\graduation-project\application\native\src\pers\kindem\watermark
-
-cd %srcLocation%
-
-java -Djava.library.path=%opencvJavaLibPath%;%opencvBinPath% -classpath %opencvJarPath% .\Main.java
