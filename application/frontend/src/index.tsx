4af448a5f506281d628b88d752ecacb3cfb729e8
John Kindem <461425614@qq.com>
2019-04-01 11:07:02 +0800

    ✨ some refactor

--- application/frontend/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { MainRouter } from './router';
-import './css/css';
-
-ReactDom.render(
-    <MainRouter/>,
-    document.getElementById('root')
-);
