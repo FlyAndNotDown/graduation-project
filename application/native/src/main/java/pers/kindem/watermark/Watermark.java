0b301256727f6253ecf3c1ed2211c896eff6b15d
John Kindem <461425614@qq.com>
2019-04-01 18:35:36 +0800

    🎉 add some code

--- application/native/src/main/java/pers/kindem/watermark/Watermark.java
+++ application/native/src/main/java/pers/kindem/watermark/Watermark.java
@@ -1,13 +1,34 @@
 package pers.kindem.watermark;
 
-public class Watermark {
-    private static final int MIN_ARGS_NUM = 10;
+import pers.kindem.watermark.tool.LogTool;
 
-    public static boolean checkArgsNum(String[] args) {
-        return args.length >= Watermark.MIN_ARGS_NUM;
-    }
+public class Watermark {
+    // const
+    private static final int MIN_ARGS_NUM = 2;
 
     public static void main(String[] args) {
-        
+        // check arg num
+        if (args.length < Watermark.MIN_ARGS_NUM) {
+            LogTool.error("too many arguments");
+            return;
+        }
+
+        // get algorithm type
+        String type = args[1];
+        if (type.equals("qdfrnt")) {
+            // get other params
+            String sourcePath;
+            String secretPath;
+            double transformOrder;
+            int transformCycle;
+            String randomMatrixType;
+            String arnoldOrder;
+            double watermarkIntensity;
+            // TODO
+        } else if (type.equals("qfrft")) {
+            return;
+        } else {
+            LogTool.error("unknown algorithm type: " + type);
+        }
     }
 }
