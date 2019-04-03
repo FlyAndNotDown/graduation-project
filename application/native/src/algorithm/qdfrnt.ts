f38032620839c69bdaeb1cfa74ef15aac2edf116
John Kindem <461425614@qq.com>
2019-04-04 00:49:37 +0800

    🎉 add some code

--- application/native/src/algorithm/qdfrnt.ts
+++ application/native/src/algorithm/qdfrnt.ts
@@ -1,3 +1,11 @@
+import * as Mathjs from 'mathjs';
+
 export class QDFRNT {
-    
+    public static dfrntKernel(order: number, cycle: number, randomMatrix: number[][]): Mathjs.Matrix {
+        // get size info
+        let rows: number = randomMatrix.length;
+        
+        // TODO
+        return null;
+    }
 }
