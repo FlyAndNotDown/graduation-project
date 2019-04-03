a792d74043d9de4f1a51860419a98fc12c9adbec
John Kindem <461425614@qq.com>
2019-04-04 00:36:20 +0800

    🎉 add some code

--- application/native/src/tool/math-tool.ts
+++ application/native/src/tool/math-tool.ts
@@ -1,5 +1,23 @@
 export class MathTool {
     public static mod(x: number, y: number): number {
+        // get mod and return
         return x - y * Math.floor(x / y);
+    }
+
+    public static getRandomMatrix(length: number): number[][] {
+        // init result
+        let result: number[][] = [];
+
+        // random
+        for (let i: number = 0; i < length; i++) {
+            let temp: number[] = [];
+            for (let j: number = 0; j < length; j++) {
+                temp.push(Math.random());
+            }
+            result.push(temp);
+        }
+
+        // return result
+        return result;
     }
 }
--- application/native/test/tool/math-tool.test.ts
+++ application/native/test/tool/math-tool.test.ts
@@ -20,4 +20,23 @@ describe('MathTool', () => {
             expect(MathTool.mod(-7, -4)).to.be.eq(-3);
         });
     });
+
+    describe('static getRandomMatrix(length: number): number[][]', () => {
+        let randomMatrix: number[][] = MathTool.getRandomMatrix(12);
+
+        it('size', () => {
+            expect(randomMatrix.length).to.be.eq(12);
+            for (let i: number = 0; i < 12; i++) {
+                expect(randomMatrix[i].length).to.be.eq(12);
+            }
+        });
+
+        it('data range', () => {
+            for (let i: number = 0; i < 12; i++) {
+                for (let j: number = 0; j < 12; j++) {
+                    expect(randomMatrix[i][j] >= 0 && randomMatrix[i][j] < 1).to.be.true;
+                }
+            }
+        });
+    });
 });
