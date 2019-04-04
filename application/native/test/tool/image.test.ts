d88ef37938af70bb9b291dd5369ccd5dbffb2f23
John Kindem <461425614@qq.com>
2019-04-04 14:33:40 +0800

    🎉 add some code

--- application/native/test/tool/image.test.ts
+++ application/native/test/tool/image.test.ts
@@ -4,25 +4,24 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
 describe('ImageTool', () => {
-    let image: Mat = new Mat(3, 4, CV_8UC3);
-    for (let i: number; i < image.rows; i++) {
-        for (let j: number; j < image.cols; j++) {
-            let temp: number = i * image.cols + j;
-            image.set(i, j, new Vec3(temp, temp, temp));
+    let source: Mat = new Mat(3, 4, CV_8UC3);
+    for (let i: number = 0; i < 3; i++) {
+        for (let j: number = 0; j < 4; j++) {
+            let temp = i * 4 + j;
+            source.set(i, j, [temp, temp, temp]);
         }
     }
-
-    let imageDouble: Mat = ImageTool.imageToDouble(image);
-    let imageUint: Mat = ImageTool.imageToUint(image);
+    let sourceDouble: Mat = ImageTool.imageToDouble(source);
+    let sourceUint: Mat = ImageTool.imageToUint(sourceDouble);
 
     describe('data source', () => {
         it('correct', () => {
-            for (let i: number; i < image.rows; i++) {
-                for (let j: number; j < image.cols; j++) {
-                    let pixel: Vec3 = <Vec3><unknown>image.at(i, j);
-                    expect(pixel.x).to.be.eq(i * image.cols + j);
-                    expect(pixel.y).to.be.eq(i * image.cols + j);
-                    expect(pixel.z).to.be.eq(i * image.cols + j);
+            for (let i: number = 0; i < 3; i++) {
+                for (let j: number = 0; j < 4; j++) {
+                    let pixel = <Vec3><unknown>source.at(i, j);
+                    expect(pixel.x).to.be.eq(i * 4 + j);
+                    expect(pixel.y).to.be.eq(i * 4 + j);
+                    expect(pixel.z).to.be.eq(i * 4 + j);
                 }
             }
         });
@@ -30,25 +29,25 @@ describe('ImageTool', () => {
 
     describe('imageToDouble()', () => {
         it('data range', () => {
-            for (let i: number; i < imageDouble.rows; i++) {
-                for (let j: number; j < imageDouble.cols; j++) {
-                    let pixel: Vec3 = <Vec3><unknown>imageDouble.at(i, j);
+            for (let i: number = 0; i < 3; i++) {
+                for (let j: number = 0; j < 4; j++) {
+                    let pixel = <Vec3><unknown>sourceDouble.at(i, j);
                     expect(pixel.x >= 0 && pixel.x <= 1).to.be.true;
-                    expect(pixel.y >= 0 && pixel.y <= 1).to.be.true;
-                    expect(pixel.z >= 0 && pixel.z <= 1).to.be.true;
+                    expect(pixel.x >= 0 && pixel.x <= 1).to.be.true;
+                    expect(pixel.x >= 0 && pixel.x <= 1).to.be.true;
                 }
             }
         });
     });
 
-    describe('imageToUint()', () => {
+    describe('imageToUint', () => {
         it('data range', () => {
-            for (let i: number; i < imageUint.rows; i++) {
-                for (let j: number; j < imageUint.cols; j++) {
-                    let pixel: Vec3 = <Vec3><unknown>imageUint.at(i, j);
+            for (let i: number = 0; i < 3; i++) {
+                for (let j: number = 0; j < 4; j++) {
+                    let pixel = <Vec3><unknown>sourceUint.at(i, j);
                     expect(pixel.x >= 0 && pixel.x <= 255).to.be.true;
-                    expect(pixel.y >= 0 && pixel.y <= 255).to.be.true;
-                    expect(pixel.z >= 0 && pixel.z <= 255).to.be.true;
+                    expect(pixel.x >= 0 && pixel.x <= 255).to.be.true;
+                    expect(pixel.x >= 0 && pixel.x <= 255).to.be.true;
                 }
             }
         });
