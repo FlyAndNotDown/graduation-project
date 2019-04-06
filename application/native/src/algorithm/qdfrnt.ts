d4cf82f0e836206a8ce31d78ea6c1eedc986809b
John Kindem <461425614@qq.com>
2019-04-06 15:10:30 +0800

    🎉 add some code

---
+++ application/native/src/algorithm/qdfrnt.ts
@@ -0,0 +1,11 @@
+export class QDFRNT {
+    public static kernel(order: number, cycle: number, randomMatrix: number[][]): number[][] {
+        // get size info
+        let rows: number = randomMatrix.length;
+
+        // get symmetrical matrix
+        // TODO
+
+        return null;
+    }
+}
