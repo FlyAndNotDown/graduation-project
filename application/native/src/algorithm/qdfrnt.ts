45fc5d3b0897abf2f106d0b9b4633053add72ad3
John Kindem <461425614@qq.com>
2019-04-06 15:18:12 +0800

    🎉 add some code

--- application/native/src/algorithm/qdfrnt.ts
+++ application/native/src/algorithm/qdfrnt.ts
@@ -1,9 +1,22 @@
+import { MatrixTool } from './../tool/matrix';
+
 export class QDFRNT {
     public static kernel(order: number, cycle: number, randomMatrix: number[][]): number[][] {
         // get size info
         let rows: number = randomMatrix.length;
+        let cols: number = randomMatrix[0].length;
+        if (rows !== cols) { throw new Error('rows & cols of random matrix should be equal'); }
 
         // get symmetrical matrix
+        let randomMatrixTransport: number[][] = MatrixTool.transport(randomMatrix);
+        let randomMatrixTransportRows: number = randomMatrixTransport.length;
+        let randomMatrixTransportCols: number = randomMatrixTransport[0].length;
+        if (randomMatrixTransportRows !== randomMatrixTransportCols) {
+            throw new Error('rows & cols of random matrix\'s transport should be equal');
+        }
+        if (randomMatrixTransportRows !== rows) {
+            throw new Error('rows of random matrix and it\'s transport should be equal');
+        }
         // TODO
 
         return null;
