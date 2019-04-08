b96fda2372eb0addf765061e79b8147eb8744792
John Kindem <461425614@qq.com>
2019-04-08 15:39:17 +0800

    🎉 add some code

--- application/native/src/algorithm/qdfrnt.ts
+++ application/native/src/algorithm/qdfrnt.ts
@@ -7,9 +7,10 @@ export class QDFRNT {
         let rows: number = random.rows;
 
         // get the random symmetrical matrix
-        let randomSym: Matrix2D = random.add(random.transport()).div(2);
+        let symMatrix: Matrix2D = random.add(random.transport()).div(2);
 
         // get the random eigen matrix
+        let eigMatrix: Matrix2D = symMatrix.eigenVectors();
         // TODO
     }
 }
