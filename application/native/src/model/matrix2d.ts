c8e96012b11e041099c0979f66fdca8901dc20ed
John Kindem <461425614@qq.com>
2019-04-08 15:08:03 +0800

    🎉 add some code

--- application/native/src/model/matrix2d.ts
+++ application/native/src/model/matrix2d.ts
@@ -111,7 +111,72 @@ export class Matrix2D {
     }
 
     public eigenVectors(): Matrix2D {
-        // TODO
-        return null;
+        const precision: number = 0.00001;
+        const iterationTime: number = 100;
+        let dimension: number = this.rows;
+
+        let matrix: Matrix2D = this.copy();
+        let diagSource: number[] = [];
+        for (let i: number = 0; i < dimension; i++) { diagSource.push(1); }
+        let eigVectors: Matrix2D = Matrix2D.diagonal(new Vector(diagSource));
+
+        let count: number = 0;
+        while (true) {
+            let max: number = matrix.get(0, 1);
+            let row: number = 0;
+            let col: number = 1;
+            for (let i: number = 0; i < dimension; i++) {
+                for (let j: number = 0; j < dimension; j++) {
+                    let temp: number = Math.abs(matrix.get(i, j));
+                    if (i !== j && temp > max) {
+                        max = temp;
+                        row = i;
+                        col = j;
+                    }
+                }
+            }
+
+            if (max < precision || count > iterationTime) { break; }
+            count++;
+
+            let app: number = matrix.get(row, row);
+            let apq: number = matrix.get(row, col);
+            let aqq: number = matrix.get(col, col);
+            let angle: number = 0.5 * Math.atan2(-2 * apq, aqq - app);
+            let sin: number = Math.sin(angle);
+            let cos: number = Math.cos(angle);
+            let sin2: number = Math.sin(2 * angle);
+            let cos2: number = Math.cos(2 * angle);
+            matrix.set(row, row, app * cos * cos + aqq * sin * sin + 2 * apq * cos * sin);
+            matrix.set(col, col, app * sin * sin + aqq * cos * cos - 2 * apq * cos * sin);
+            matrix.set(row, col, 0.5 * (aqq - app) * sin2 + apq * cos2);
+            matrix.set(col, row, matrix.get(row, col));
+
+            for (let i: number = 0; i < dimension; i++) {
+                if (i !== col && i !== row) {
+                    max = matrix.get(i, row);
+                    matrix.set(i, row, matrix.get(i, col) * sin + max * cos);
+                    matrix.set(i, col, matrix.get(i, col) * cos - max * sin);
+                }
+            }
+
+            for (let i: number = 0; i < dimension; i++) {
+                if (i !== col && i !== row) {
+                    max = matrix.get(row, i);
+                    matrix.set(row, i, matrix.get(col, i) * sin + max * cos);
+                    matrix.set(col, i, matrix.get(col, i) * cos - max * sin);
+                }
+            }
+
+            for (let i: number = 0; i < dimension; i++) {
+                max = eigVectors.get(i, row);
+                eigVectors.set(i, row, eigVectors.get(i, col) * sin + max * cos);
+                eigVectors.set(i, col, eigVectors.get(i, col) * cos - max * sin);
+            }
+        }
+
+        // get eigValues
+        // @see https://blog.csdn.net/webzhuce/article/details/85013301
+        return eigVectors;
     }
 }
