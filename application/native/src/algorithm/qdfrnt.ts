f6a8053c569305dc43783d6f3d67263b2d4fcc66
John Kindem <461425614@qq.com>
2019-04-10 16:31:09 +0800

    🎉 add some code

--- application/native/src/algorithm/qdfrnt.ts
+++ application/native/src/algorithm/qdfrnt.ts
@@ -1,5 +1,5 @@
 import { Vector } from './../model/vector';
-import { ComplexMatrix2D, ConvertToComplexVectorArrayType } from './../model/complex-matrix2d';
+import { ComplexMatrix2D, ConvertToComplexVectorArrayType, RestoreFromComplexVectorArrayType } from './../model/complex-matrix2d';
 import { Complex } from './../model/complex';
 import { Matrix2D } from './../model/matrix2d';
 import { ComplexVector } from '../model/complex-vector';
@@ -32,5 +32,21 @@ export class QDFRNT {
         let sourceTransport: ComplexMatrix2D = source.convertToComplexMatrix2D().transport();
         let result: ComplexMatrix2D = kernel.mul(sourceTransport).transport();
         return result.convertToComplexVectorArray(ConvertToComplexVectorArrayType.RowAsVector)[0];
+    }
+
+    public static dfrnt2(source: ComplexMatrix2D, kernelRow: ComplexMatrix2D, kernelCol: ComplexMatrix2D): ComplexMatrix2D {
+        let rowVectors: ComplexVector[] = source.convertToComplexVectorArray(ConvertToComplexVectorArrayType.RowAsVector);
+        let rowOutputVectors: ComplexVector[] = [];
+        for (let i: number = 0; i < rowVectors.length; i++) {
+            rowOutputVectors.push(QDFRNT.dfrnt(rowVectors[i], kernelRow));
+        }
+        let output: ComplexMatrix2D = ComplexMatrix2D.restoreFromComplexVectorArray(RestoreFromComplexVectorArrayType.RowAsVector, rowOutputVectors);
+        let colVectors: ComplexVector[] = output.convertToComplexVectorArray(ConvertToComplexVectorArrayType.ColAsVector);
+        let colOutputVectors: ComplexVector[] = [];
+        for (let i: number = 0; i < colVectors.length; i++) {
+            colOutputVectors.push(QDFRNT.dfrnt(colVectors[i], kernelCol));
+        }
+
+        return ComplexMatrix2D.restoreFromComplexVectorArray(RestoreFromComplexVectorArrayType.ColAsVector, colOutputVectors);
     }
 }
--- application/native/src/main.ts
+++ application/native/src/main.ts
@@ -3,12 +3,27 @@ import { Vector } from './model/vector';
 import { MatrixTool } from './tool/matrix';
 import { QDFRNT } from './algorithm/qdfrnt';
 import { ComplexMatrix2D } from './model/complex-matrix2d';
+import { Complex } from './model/complex';
 
-let source: ComplexVector = new Vector([1, 2, 3, 4]).convertToComplexVector();
-let kernel: ComplexMatrix2D = QDFRNT.kernel(0.25, 1, MatrixTool.getRandomSquareMatrix(4));
-let result1: ComplexVector = QDFRNT.dfrnt(source, kernel);
-let result2: ComplexVector = QDFRNT.dfrnt(result1, kernel);
-let result3: ComplexVector = QDFRNT.dfrnt(result2, kernel);
-let result4: ComplexVector = QDFRNT.dfrnt(result3, kernel);
+// let source: ComplexVector = new Vector([1, 2, 3, 4]).convertToComplexVector();
+// let kernel: ComplexMatrix2D = QDFRNT.kernel(0.25, 1, MatrixTool.getRandomSquareMatrix(4));
+// let result1: ComplexVector = QDFRNT.dfrnt(source, kernel);
+// let result2: ComplexVector = QDFRNT.dfrnt(result1, kernel);
+// let result3: ComplexVector = QDFRNT.dfrnt(result2, kernel);
+// let result4: ComplexVector = QDFRNT.dfrnt(result3, kernel);
+
+let source: ComplexMatrix2D = new ComplexMatrix2D([
+    [new Complex(1, 0), new Complex(2, 0), new Complex(3, 0), new Complex(4, 0)],
+    [new Complex(5, 0), new Complex(6, 0), new Complex(7, 0), new Complex(8, 0)],
+    [new Complex(9, 0), new Complex(10, 0), new Complex(11, 0), new Complex(12, 0)],
+    [new Complex(13, 0), new Complex(14, 0), new Complex(15, 0), new Complex(16, 0)]
+]);
+let kernelRow: ComplexMatrix2D = QDFRNT.kernel(0.25, 1, MatrixTool.getRandomSquareMatrix(4));
+let kernelCol: ComplexMatrix2D = QDFRNT.kernel(0.25, 1, MatrixTool.getRandomSquareMatrix(4));
+let result1: ComplexMatrix2D = QDFRNT.dfrnt2(source, kernelRow, kernelCol);
+let result2: ComplexMatrix2D = QDFRNT.dfrnt2(result1, kernelRow, kernelCol);
+let result3: ComplexMatrix2D = QDFRNT.dfrnt2(result2, kernelRow, kernelCol);
+let result4: ComplexMatrix2D = QDFRNT.dfrnt2(result3, kernelRow, kernelCol);
+
 
 console.log('over');
