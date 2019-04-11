5878e26644fcc07cd899320155fe8e1eef1261b5
John Kindem <461425614@qq.com>
2019-04-11 19:31:21 +0800

    🎉 add some code

--- application/native/src/main.ts
+++ application/native/src/main.ts
@@ -1,3 +1,4 @@
+import { ComplexVectorWithChannels } from './model/complex-vector-with-channels';
 import { ComplexVector } from './model/complex-vector';
 import { Vector } from './model/vector';
 import { MatrixTool } from './tool/matrix';
@@ -12,18 +13,31 @@ import { Complex } from './model/complex';
 // let result3: ComplexVector = QDFRNT.dfrnt(result2, kernel);
 // let result4: ComplexVector = QDFRNT.dfrnt(result3, kernel);
 
-let source: ComplexMatrix2D = new ComplexMatrix2D([
-    [new Complex(1, 0), new Complex(2, 0), new Complex(3, 0), new Complex(4, 0)],
-    [new Complex(5, 0), new Complex(6, 0), new Complex(7, 0), new Complex(8, 0)],
-    [new Complex(9, 0), new Complex(10, 0), new Complex(11, 0), new Complex(12, 0)],
-    [new Complex(13, 0), new Complex(14, 0), new Complex(15, 0), new Complex(16, 0)]
-]);
-let kernelRow: ComplexMatrix2D = QDFRNT.kernel(0.25, 1, MatrixTool.getRandomSquareMatrix(4));
-let kernelCol: ComplexMatrix2D = QDFRNT.kernel(0.25, 1, MatrixTool.getRandomSquareMatrix(4));
-let result1: ComplexMatrix2D = QDFRNT.dfrnt2(source, kernelRow, kernelCol);
-let result2: ComplexMatrix2D = QDFRNT.dfrnt2(result1, kernelRow, kernelCol);
-let result3: ComplexMatrix2D = QDFRNT.dfrnt2(result2, kernelRow, kernelCol);
-let result4: ComplexMatrix2D = QDFRNT.dfrnt2(result3, kernelRow, kernelCol);
+// let source: ComplexMatrix2D = new ComplexMatrix2D([
+//     [new Complex(1, 0), new Complex(2, 0), new Complex(3, 0), new Complex(4, 0)],
+//     [new Complex(5, 0), new Complex(6, 0), new Complex(7, 0), new Complex(8, 0)],
+//     [new Complex(9, 0), new Complex(10, 0), new Complex(11, 0), new Complex(12, 0)],
+//     [new Complex(13, 0), new Complex(14, 0), new Complex(15, 0), new Complex(16, 0)]
+// ]);
+// let kernelRow: ComplexMatrix2D = QDFRNT.kernel(0.25, 1, MatrixTool.getRandomSquareMatrix(4));
+// let kernelCol: ComplexMatrix2D = QDFRNT.kernel(0.25, 1, MatrixTool.getRandomSquareMatrix(4));
+// let result1: ComplexMatrix2D = QDFRNT.dfrnt2(source, kernelRow, kernelCol);
+// let result2: ComplexMatrix2D = QDFRNT.dfrnt2(result1, kernelRow, kernelCol);
+// let result3: ComplexMatrix2D = QDFRNT.dfrnt2(result2, kernelRow, kernelCol);
+// let result4: ComplexMatrix2D = QDFRNT.dfrnt2(result3, kernelRow, kernelCol);
 
+let source: ComplexVectorWithChannels = new ComplexVectorWithChannels([
+    [new Complex(1, 1), new Complex(2, 2), new Complex(3, 3), new Complex(4, 4)],
+    [new Complex(5, 5), new Complex(6, 6), new Complex(7, 7), new Complex(8, 8)],
+    [new Complex(9, 9), new Complex(10, 10), new Complex(11, 11), new Complex(12, 12)],
+    [new Complex(13, 13), new Complex(14, 14), new Complex(15, 15), new Complex(16, 16)],
+    [new Complex(17, 17), new Complex(18, 18), new Complex(19, 19), new Complex(20, 20)]
+]);
+let kernel: ComplexMatrix2D = QDFRNT.kernel(0.25, 1, MatrixTool.getRandomSquareMatrix(5));
+let quaternion: Vector = new Vector([0, 1, 0, 0]);
+let result1: ComplexVectorWithChannels = QDFRNT.lqdfrnt(source, kernel, quaternion);
+let result2: ComplexVectorWithChannels = QDFRNT.lqdfrnt(result1, kernel, quaternion);
+let result3: ComplexVectorWithChannels = QDFRNT.lqdfrnt(result2, kernel, quaternion);
+let result4: ComplexVectorWithChannels = QDFRNT.lqdfrnt(result3, kernel, quaternion);
 
 console.log('over');
