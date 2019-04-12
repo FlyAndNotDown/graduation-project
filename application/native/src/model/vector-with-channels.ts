abbcca5b87516b59a3aded790866a39f017b793d
John Kindem <461425614@qq.com>
2019-04-12 12:39:25 +0800

    🎉 add some code

--- application/native/src/model/vector-with-channels.ts
+++ application/native/src/model/vector-with-channels.ts
@@ -1,3 +1,4 @@
+import { Vector } from './vector';
 export class VectorWithChannels {
     private data: number[][];
     public length: number;
@@ -7,5 +8,51 @@ export class VectorWithChannels {
         this.data = data;
         this.length = data.length;
         this.channels = data[0].length;
+    }
+
+    public static zeros(length: number, channels: number): VectorWithChannels {
+        let data: number[][] = [];
+        for (let i: number = 0; i < length; i++) {
+            let pixel: number[] = [];
+            for (let j: number = 0; j < channels; j++) {
+                pixel.push(0);
+            }
+            data.push(pixel);
+        }
+        return new VectorWithChannels(data);
+    }
+
+    public get(i: number, j: number): number {
+        return this.data[i][j];
+    }
+
+    public set(i: number, j: number, item: number): void {
+        this.data[i][j] = item;
+    }
+
+    public getData(): number[][] {
+        return this.data;
+    }
+
+    public copy(): VectorWithChannels {
+        let result: VectorWithChannels = VectorWithChannels.zeros(this.length, this.channels);
+        for (let i: number = 0; i < this.length; i++) {
+            for (let j: number = 0; j < this.channels; j++) {
+                result.set(i, j, this.get(i, j));
+            }
+        }
+        return result;
+    }
+
+    public convertToVectorArray(): Vector[] {
+        let result: Vector[] = [];
+        for (let i: number = 0; i < this.channels; i++) {
+            let vector: Vector = Vector.zeros(this.length);
+            for (let j: number = 0; j < this.length; j++) {
+                vector.set(j, this.get(j, i));
+            }
+            result.push(vector);
+        }
+        return result;
     }
 }
