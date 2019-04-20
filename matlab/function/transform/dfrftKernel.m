eeb988b300dc25571462ae2091e9d84702195c58
John Kindem <461425614@qq.com>
2019-04-21 05:39:54 +0800

    🎉 add some code

--- matlab/function/transform/dfrftKernel.m
+++ matlab/function/transform/dfrftKernel.m
@@ -1,9 +1,20 @@
 function output = dfrftKernel(a, len)
+%dfrftKernel - get kernel matrix of DFrFT with GSA/OPA method
+%
+% - Arguments:
+%       - a [double] fractional order of transform
+%       - len [int] length of matrix
+%
+% - Returns:
+%       - output [nxn matrix] kernel matrix of DFrFT
 
+% judge if it is even number
 even = ~rem(len, 2);
 
-dftEigens = getDftEigens(len);
+% get DFT eigen vectors
+dftEigens = dftEigenVectors(len);
 
+% calculate matrix U
 matrixU = zeros(len, len);
 for n1 = 1 : len
     u = hermiteSample(n1 - 1, len);
@@ -29,6 +40,7 @@ if even
 end
 matrixU = orth(matrixU);
 
+% calculate matrix D
 matrixD = zeros(len, len);
 for n = 1 : len
     matrixD(n, n) = exp(-1i * a * (n - 1));
@@ -37,59 +49,7 @@ if even
     matrixD(n, n) = exp(-1i * a * (len));
 end
 
+% get kernel matrix
 output = matrixU * matrixD * matrixU';
-
-end
-
-
-
-function output = hermiteSample(nth, len)
-
-even = ~rem(len, 2);
-t = sqrt(2 * pi / len);
-output = zeros(len, 1);
-publicFactor = 1 / sqrt(2 ^ nth * factorial(nth) * sqrt(len / 2) * t);
-xSymbol = sym('x');
-her = hermiteH(nth, xSymbol);
-for n = 1 : len
-    k = n - (floor(len / 2) + 1);
-    x = k / sqrt(len / (2 * pi));
-    output(n, 1) = publicFactor * eval(her) * exp(0 - k * k * pi / len);
-end
-
-if even
-    front = output(1 : floor(len / 2), 1);
-    back = output(floor(len / 2) + 1 : len, 1);
-    output(1 : floor(len / 2), 1) = back;
-    output(floor(len / 2) + 1 : len, 1) = front;
-else
-    front = output(1 : floor(len / 2), 1);
-    back =  output(floor(len / 2) + 1 : len, 1);
-    output(1 : floor(len / 2) + 1, 1) = back;
-    output(floor(len / 2) + 2 : len, 1) = front;
-end
-
-vectorMod = norm(output);
-output = output / vectorMod;
-
-end
-
-
-
-function output = getDftEigens(len)
-
-s = zeros(len, len);
-s(1, len) = 1;
-s(len, 1) = 1;
-w = 2 * pi / len;
-for n = 1 : len
-    s(n, n) = 2 * cos((n - 1) * w);
-end
-for n = 1 : len - 1
-    s(n, n + 1) = 1;
-    s(n + 1, n) = 1;
-end
-
-[output, ~] = eig(s);
 
 end
