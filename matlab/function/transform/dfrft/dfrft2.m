76dc76ddd6d9e5c2b794592d3566a0457724ba67
John Kindem <461425614@qq.com>
2019-04-21 19:37:18 +0800

    🎉 add some code

--- matlab/function/transform/dfrft/dfrft2.m
+++ matlab/function/transform/dfrft/dfrft2.m
@@ -1,12 +1,19 @@
-function output = dfrft2(source, rows, cols, a)
+function output = dfrft2(source, sRows, sCols, a)
+%dfrft2 - two-dimension DFrFT
+%
+% - Arguments:
+%       - source [nxn matrix] source signal matrix
+%       - sRows [int] sRows count
+%       - sCols [int] sCols count
+%       - a [double] fractional order of DFrFT
 
 output = source;
 
-for n = 1 : rows
+for n = 1 : sRows
     output(n, :) = DFpei(output(n, :)', a)';
 end
 
-for n = 1 : cols
+for n = 1 : sCols
     output(:, n) = DFpei(output(:, n), a);
 end
 
