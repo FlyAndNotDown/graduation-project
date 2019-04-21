9422cdcf8217958958c5549d64493f8c661c6f9f
John Kindem <461425614@qq.com>
2019-04-21 13:42:02 +0800

    🎉 add some code

--- matlab/test/transform/dfrftTest.m
+++ matlab/test/transform/dfrftTest.m
@@ -6,16 +6,23 @@ for n = 1 : length(source)
 end
 x = 1:1:length(source);
 
-kernel = dfrftKernel(3 * pi / 7, length(source));
-iKernel = dfrftKernel(-3 * pi / 7, length(source));
+kernel = dfrftKernel(0.5, length(source));
+iKernel = dfrftKernel(-0.5, length(source));
 
 source = source';
 
 output = dfrft(source, kernel);
 restored = dfrft(output, iKernel);
 
+output2 = DFpei(source, 0.5);
+restored2 = DFpei(output2, -0.5);
+
 figure(1);
 subplot(2, 2, 1);
 plot(x, real(output));
 subplot(2, 2, 2);
 plot(x, imag(output));
+subplot(2, 2, 3);
+plot(x, real(output2));
+subplot(2, 2, 4);
+plot(x, imag(output2));
