6cec21f0205da71bb25368e278aeb3f228d283f0
John Kindem <461425614@qq.com>
2019-04-23 14:34:21 +0800

    🎉 add some code

---
+++ matlab/test/watermark/qdfrftMarkTest.m
@@ -0,0 +1,29 @@
+% load image
+source = im2double(imread('lena.bmp'));
+secret = imread('secret.bmp');
+
+% calculate the kernel DFrFT
+kernel = dfrftKernel(8, 0.5);
+iKernel = dfrftKernel(8, -0.5);
+
+% watermarking
+[output, kp] = qdfrftMark(source, secret, 3, kernel, iKernel, 0.05);
+
+% calculate ssim
+[ssimVal, ~] = ssim(source, output);
+
+% restore
+load('data/qdfrftModel.mat', 'model');
+secretRestored = qdfrftRestore(output, model, kp, 3, r);
+secretBer = ber(secret, secretRestored);
+
+% show result
+figure();
+subplot(2, 2, 1);
+imshow(source);
+subplot(2, 2, 2);
+imshow(secret);
+subplot(2, 2, 3);
+imshow(output);
+subplot(2, 2, 4);
+imshow(secretRestored);
---
+++ matlab/test/watermark/qdfrftTrainTest.m
@@ -0,0 +1,12 @@
+% load image
+source = im2double(imread('lena.bmp'));
+
+% init secret sequence
+secret = floor(rand(1, 2000) + 0.5);
+
+% calculate the kernel of DFrFT
+kernel = dfrftKernel(8, 0.5);
+
+% start train
+model = qdfrftTrain(source, secret, kernel, 0.05);
+save('data/qdfrftModel.mat', 'model');
