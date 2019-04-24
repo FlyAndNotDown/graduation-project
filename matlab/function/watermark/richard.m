18d98a782c4a37c241bddd87d5be97478ad4ba0d
John Kindem <461425614@qq.com>
2019-04-24 14:09:59 +0800

    🛠 fix a bug

--- matlab/function/watermark/richard.m
+++ matlab/function/watermark/richard.m
@@ -5,9 +5,9 @@ function output = richard(keys, t)
 key1 = keys(1, 1 : 6);
 key2 = keys(1, 7 : 12);
 key3 = keys(1, 13 : 18);
-if t <= 0.3
+if t <= 1.5
     key = key1;
-elseif t > 0.7
+elseif t > 3.5
     key = key3;
 else
     key = key2;
@@ -21,5 +21,6 @@ v = key(1, 6);
 
 % calculate
 output = a + (k - a) / ((1 + q * exp(0 - b * (t - m))) ^ (1 / v));
+output = real(output);
 
 end
