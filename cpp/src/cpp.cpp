ce10d93352a206377c7c6a2d27c091efcccc0b3f
John Kindem <461425614@qq.com>
2019-04-24 22:36:42 +0800

    🎉 add some code

---
+++ cpp/src/cpp.cpp
@@ -0,0 +1,18 @@
+﻿#include "cpp.h"
+#include <armadillo>
+
+using namespace arma;
+using namespace std;
+
+int main()
+{
+	mat A = randu<mat>(5, 10);
+	mat C = randu<mat>(10, 5);
+	mat U = A * C;
+	U.print("U:");
+	mat V = inv(U);
+	V.print("V:");
+	double D = det(U);
+	cout << "det(U)=" << D << endl;
+	return 0;
+}
