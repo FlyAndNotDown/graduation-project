96a87c9de9b9e2353d30b06f04578692569dce7d
John Kindem <461425614@qq.com>
2019-04-25 13:41:27 +0800

    🎉 add some code

--- matlab/function/watermark/qdfrftQcTrain.m
+++ matlab/function/watermark/qdfrftQcTrain.m
@@ -1,5 +1,6 @@
 function gaKeys = qdfrftQcTrain()
 
-gaKeys = ga(@fitness, 18);
+options = optimoptions('ga', 'PopulationSize', 20, 'EliteCount', 0.01, 'CrossoverFraction', 0.6, 'Generations', 50);
+gaKeys = ga(@fitness, 18, [], [], [], [], 0, 10, [], options);
 
 end
