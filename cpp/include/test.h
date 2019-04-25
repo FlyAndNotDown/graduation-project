f67abbcac9a454519f767eae55d5d20f3a9ce727
John Kindem <461425614@qq.com>
2019-04-25 18:43:05 +0800

    🎉 add some code

---
+++ cpp/include/test.h
@@ -0,0 +1,14 @@
+#ifndef WATERMARK_TEST_H
+#define WATERMARK_TEST_H
+
+namespace watermark {
+    class test {
+        public:
+            // tool
+            static void tool_test_print_mat();
+            static void tool_test_print_cx_mat();
+            static void tool_test_mat_to_cx_mat();
+    };
+};
+
+#endif
