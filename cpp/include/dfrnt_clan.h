4c28e28aed906badf1ee914510db389eb8655570
John Kindem <461425614@qq.com>
2019-04-25 16:03:17 +0800

    🎉 add some code

--- cpp/include/dfrnt_clan.h
+++ cpp/include/dfrnt_clan.h
@@ -10,8 +10,8 @@ namespace watermark {
             static cx_mat kernel(double order, double cycle, mat random_matrix);
             static cx_vec dfrnt(cx_vec source, cx_mat kernel);
             static cx_mat dfrnt2(cx_mat source, unsigned int length, cx_mat kernel);
-            static cx_mat lqdfrnt(cx_mat source, cx_mat kernel, vec unit_pure_quaternion);
-            static cx_cube lqdfrnt2(cx_cube source, unsigned int length, cx_mat kernel, vec unit_pure_quaternion);
+            static cube lqdfrnt(cube source, cx_mat kernel, vec unit_pure_quaternion);
+            static cube lqdfrnt2(cube source, unsigned int length, cx_mat kernel, vec unit_pure_quaternion);
     };
 };
 
--- cpp/src/dfrnt_clan.cpp
+++ cpp/src/dfrnt_clan.cpp
@@ -28,3 +28,65 @@ cx_mat dfrnt_clan::kernel(double order, double cycle, mat random_matrix) {
     // get kernel matrix
     return orth_vectors * center_matrix * orth_vectors.t();
 }
+
+cx_vec dfrnt_clan::dfrnt(cx_vec source, cx_mat kernel) {
+    // return result
+    return kernel * source;
+}
+
+cx_mat dfrnt_clan::dfrnt2(cx_mat source, unsigned int length, cx_mat kernel) {
+    // init output
+    cx_mat output = source;
+
+    // do dfrnt to every row
+    for (unsigned int i = 0; i < length; i++) {
+        output.row(i) = dfrnt_clan::dfrnt(output.row(i).t(), kernel).t();
+    }
+
+    // do dfrnt to every col
+    for (unsigned int i = 0; i < length; i++) {
+        output.col(i) = dfrnt_clan::dfrnt(output.col(i), kernel);
+    }
+
+    // return result
+    return output;
+}
+
+cube dfrnt_clan::lqdfrnt(cube source, cx_mat kernel, vec unit_pure_quaternion) {
+    // get every channel of vector
+    auto source_r = source.slice(0);
+    auto source_i = source.slice(1);
+    auto source_j = source.slice(2);
+    auto source_k = source.slice(3);
+
+    // get three imag part of unit pure quaternion
+    auto ua = unit_pure_quaternion.at(1);
+    auto ub = unit_pure_quaternion.at(2);
+    auto uc = unit_pure_quaternion.at(3);
+
+    // do the 1-d dfrnt to every channel
+    auto output_r = dfrnt_clan::dfrnt(source_r, kernel);
+    auto output_i = dfrnt_clan::dfrnt(source_i, kernel);
+    auto output_j = dfrnt_clan::dfrnt(source_j, kernel);
+    auto output_k = dfrnt_clan::dfrnt(source_k, kernel);
+    
+    // get real part and imag part
+    mat output_real_r = real(output_r);
+    mat output_real_i = real(output_i);
+    mat output_real_j = real(output_j);
+    mat output_real_k = real(output_k);
+    mat output_imag_r = imag(output_r);
+    mat output_imag_i = imag(output_i);
+    mat output_imag_j = imag(output_j);
+    mat output_imag_k = imag(output_k);
+
+    // init output
+    auto output = source;
+    output.slice(0) = output_real_r - output_imag_i * ua - output_imag_j * ub - output_imag_k * uc;
+    output.slice(1) = output_real_i + output_imag_r * ua - output_imag_j * uc + output_imag_k * ub;
+    output.slice(2) = output_real_j + output_imag_r * ub - output_imag_k * ua + output_imag_i * uc;
+    output.slice(3) = output_real_k + output_imag_r * uc - output_imag_i * ub + output_imag_j * ua;
+
+    // return result
+    return output;
+}
