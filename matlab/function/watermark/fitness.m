414ad2d1426133ca951c7381fc618b9da221b5ab
John Kindem <461425614@qq.com>
2019-04-26 15:01:00 +0800

    🛠 fix a bug

--- matlab/function/watermark/fitness.m
+++ matlab/function/watermark/fitness.m
@@ -1,4 +1,4 @@
-function output = fitness(keys)
+function output = fitness(intensityFactor)
 %fitness - GA fitness function
 
 % bers = [0.0547, 0.1057, 0.0615, 0.0291, 0.1431, 0.0896, 0.0168, 0.0603, 0.0867, 0];
@@ -9,7 +9,7 @@ secret = im2double(imread('secret.bmp'));
 kernel = dfrftKernel(8, 0.5);
 iKernel = dfrftKernel(8, -0.5);
 
-[sourceMarked, kp, kl] = qdfrftQcMark(source, secret, 3, kernel, iKernel, keys);
+[sourceMarked, kp, kl] = qdfrftQcMark(source, secret, 3, kernel, iKernel, intensityFactor);
 secretRestored = qdfrftQcRestore(sourceMarked, kp, kl, 3, kernel);
 [ssimVal, ~] = ssim(source, sourceMarked);
 secretBers = ber(secret, secretRestored);
