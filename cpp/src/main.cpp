2d26763e49dd22139364f749900daaad63161429
John Kindem <461425614@qq.com>
2019-04-26 19:09:12 +0800

    🎉 add some code

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -3,8 +3,8 @@ using namespace watermark;
 
 int main(int argc, char *argv[]) {
 	test::tool_read_write_color_image(
-		"C:/Users/Administrator/Desktop/001.jpg",
-		"C:/Users/Administrator/Desktop/002.jpg"
+		"C:/Users/Administrator/Desktop/lena.bmp",
+		"C:/Users/Administrator/Desktop/lena-output.bmp"
 	);
 
     return 0;
