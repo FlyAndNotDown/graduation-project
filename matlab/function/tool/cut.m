74ce1491c684513d56c36f244e9ab51806f9a3a8
John Kindem <461425614@qq.com>
2019-05-14 19:03:19 +0800

    🎉 add some code

--- matlab/function/tool/cut.m
+++ matlab/function/tool/cut.m
@@ -7,5 +7,14 @@ function output = cut(source, percent)
 % - Returns:
 %       - output [?x? matrix] output image after cut
 
+% get size info
+[sourceRows, sourceCols, sourceChannels] = size(source);
+
+% get cut length
+cutLength = floor(sourceCols * percent);
+
+% do cut
+output = source;
+output(:, sourceCols - cutLength + 1 : sourceRows, :) = 0;
 
 end
