08f8a0bd1bb49d48975e117f191771dfd8bb8aec
John Kindem <461425614@qq.com>
2019-05-15 15:00:33 +0800

    🎉 add some code

---
+++ matlab/test/performance/qdfrnt/qdfrntResizeTest.m
@@ -0,0 +1,32 @@
+source = im2double(imread('lena.bmp'));
+secret = imread('secret.bmp');
+
+[sourceRows, sourceCols, ~] = size(source);
+
+indexFile = fopen('dist/index.txt', 'w+');
+
+p = rand(8, 8);
+r = dfrntKernel(0.75, 1, p);
+ir = dfrntKernel(-0.75, 1, p);
+
+load('data/qdfrntModel.mat', 'model');
+
+secretBers = zeros(1, 10);
+x = zeros(1, 10);
+
+fprintf(indexFile, 'testNo\t\tresizePercent\t\tber\n');
+for n = 1 : 10
+    x(1, n) = 0.05 * n;
+    [output, kp] = qdfrntMark(source, secret, 3, r, ir, 13);
+    output = imresize(output, 1 - x(1, n));
+    output = imresize(output, [sourceRows, sourceRows]);
+    secretRestored = qdfrntRestore(output, model, kp, 3, r);
+    secretBers(1, n) = ber(secret, secretRestored);
+
+    imwrite(output, ['dist/', num2str(n), '-output.bmp']);
+    imwrite(secretRestored, ['dist/', num2str(n), '-restored.bmp']);
+    fprintf(indexFile, '%d\t\t\t%f\t\t\t\t%f\n', n, x(1, n), secretBers(1, n));
+end
+
+fclose(indexFile);
+plot(x, secretBers);
